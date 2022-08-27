f6d35f394dd26970d050d4540602922dd8a0c639
Salokya <kumar.salokya007@gmail.com>
2022-08-27 23:15:54 +0530

    added without Securty WebSecurityConfigurerAdapter class

--- src/main/java/com/springboot/blog/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/springboot/blog/security/JwtAuthenticationEntryPoint.java
@@ -1,18 +1,24 @@
 package com.springboot.blog.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
+@Component("restAuthenticationEntryPoint")
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver resolver;
+
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        resolver.resolveException(request, response, null, exception);
     }
 }
