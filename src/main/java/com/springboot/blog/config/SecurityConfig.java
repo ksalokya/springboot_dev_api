723108d30984156e252a8077679e3d56194ad348
Salokya <kumar.salokya007@gmail.com>
2022-08-27 21:27:52 +0530

    added database authentication

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.springboot.blog.config;
 
+import com.springboot.blog.security.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,6 +29,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -43,16 +48,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails root = User.builder().username("root")
-                .password(passwordEncoder()
-                .encode("root")).roles("USER").build();
-
-        UserDetails admin = User.builder().username("admin")
-                .password(passwordEncoder()
-                .encode("admin")).roles("ADMIN").build();
-
-        return new InMemoryUserDetailsManager(root, admin);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailService).passwordEncoder(passwordEncoder());
     }
+
+    /*
+     * IN MEMORY
+     * */
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails root = User.builder().username("root")
+//                .password(passwordEncoder()
+//                .encode("root")).roles("USER").build();
+//
+//        UserDetails admin = User.builder().username("admin")
+//                .password(passwordEncoder()
+//                .encode("admin")).roles("ADMIN").build();
+//
+//        return new InMemoryUserDetailsManager(root, admin);
+//    }
 }
